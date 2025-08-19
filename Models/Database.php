c0b47352b5490b0bfd2575b021129c4bf2fc73bb
nzomutchawilfrid <nzomutchawilfrid@gmail.com>
2025-08-19 17:57:06 +0100

    update

--- Models/Database.php
+++ Models/Database.php
@@ -33,5 +33,20 @@ class Database {
         }
         return $this->conn;
     }
+
+    // public function getConnection() {
+    //     $this->conn = null;
+    //     try {
+    //         $this->conn = new PDO(
+    //             "mysql:host={$this->host};dbname={$this->db_name};charset=utf8mb4",
+    //             $this->username,
+    //             $this->password
+    //         );
+    //         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     } catch(PDOException $exception){
+    //         echo "Erreur de connexion : " . $exception->getMessage();
+    //     }
+    //     return $this->conn;
+    // }
 }
 ?>
